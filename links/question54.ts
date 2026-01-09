857af3dcc7db0e737b31963e5c8afa07bd52c4ef
vonnue-assin <assin.anna@vonnue.com>
2026-01-09 09:48:15 +0530

    feat:implemented new functionalities

---
+++ links/question54.ts
@@ -0,0 +1,2 @@
+//Write a JavaScript program to create a class called "Rectangle" with properties for width and height.Include two methods to calculate rectangle area and perimeter.Create an instance of the "Rectangle" class and calculate its area and perimeter.
+
